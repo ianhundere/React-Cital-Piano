0cb7b900bb1169598a533d9fcf65589424565c08
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-16 17:52:24 -0500

    styles sheet music component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Piano from './Piano';
+import Sheetmusic from './Sheetmusic';
 
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <Sheetmusic />
                 <Piano />
             </div>
         );
---
+++ src/Sheetmusic.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sheetmusic = (props) => {
+    
+    return (
+
+
+        <div className="sheetmusic">
+            <h2>Jingle Bells</h2>
+        </div>
+    )
+}
+
+export default Sheetmusic;
