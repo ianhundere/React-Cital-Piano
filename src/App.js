78bf107692058f3c92695734221e18876d405e37
Ian Hundere <ianfundere@gmail.com>
2018-12-17 11:47:32 -0500

    moves keyTranslation to app.js

--- src/App.js
+++ src/App.js
@@ -8,21 +8,40 @@ class App extends Component {
         console.log(document);
         super(props);
         this.state = {
-            key: ''
+            key: []
+        };
+        this.keyTranslation = {
+            a: 'B3',
+            s: 'C4',
+            e: 'C#4',
+            d: 'D4',
+            r: 'D#4',
+            f: 'E4',
+            g: 'F4',
+            y: 'F#4',
+            h: 'G4',
+            u: 'G#4',
+            j: 'A4',
+            i: 'A#4',
+            k: 'B4'
         };
     }
     componentDidMount() {
         document.addEventListener('keydown', e => {
             this.setState({
-                key: e.key
+                key: [...this.state.key, this.keyTranslation[e.key]]
             });
-            console.log(e.key);
+            console.log();
+            // console.log(e.key);
         });
     }
     render() {
         return (
             <div className="App">
-                <Piano pianoKey={this.state.key} />
+                <Piano
+                    pianoKey={this.state.key}
+                    allKeys={Object.values(this.keyTranslation)}
+                />
             </div>
         );
     }
--- src/Piano.js
+++ src/Piano.js
@@ -6,29 +6,9 @@ import Key from './Key';
 class Piano extends Component {
     constructor(props) {
         super(props);
-        const reverb = new Tone.Freeverb(0.7).toMaster();
-        const filter = new Tone.Filter().toMaster();
-        this.synth = new Tone.PolySynth()
-            .chain(filter)
-            .chain(Tone.Master)
-            .chain(reverb)
-            .chain(filter)
-            .toMaster();
-        this.keyTranslation = {
-            a: 'B3',
-            s: 'C4',
-            e: 'C#4',
-            d: 'D4',
-            r: 'D#4',
-            f: 'E4',
-            g: 'F4',
-            y: 'F#4',
-            h: 'G4',
-            u: 'G#4',
-            j: 'A4',
-            i: 'A#4',
-            k: 'B4'
-        };
+        const reverb = new Tone.Freeverb(0.7);
+        const filter = new Tone.Filter(800, 'lowpass').toMaster();
+        this.synth = new Tone.PolySynth().chain(filter).chain(reverb);
     }
 
     noteOn = note => {
@@ -37,14 +17,12 @@ class Piano extends Component {
     noteOff = note => {
         this.synth.triggerRelease(note);
     };
+
     render() {
         if (this.props.pianoKey) {
-            console.log(this.props.pianoKey);
-            this.synth.triggerAttackRelease(
-                this.keyTranslation[this.props.pianoKey]
-            );
+            this.synth.triggerAttackRelease(this.props.pianoKey);
         }
-        const keyList = Object.values(this.keyTranslation).map(key => (
+        const keyList = this.props.allKeys.map(key => (
             <Key
                 key={key}
                 synth={this.synth}
