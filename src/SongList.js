5ef4df993fbaea0968da5e40c0459c5a7c77729d
Ian Hundere <ianfundere@gmail.com>
2018-12-20 07:35:54 -0500

    adds href to anchor tag

--- src/SongList.js
+++ src/SongList.js
@@ -1,23 +1,26 @@
 import React from 'react';
 
-const SongList = (props) => {
-    console.log(props.songs)
+const SongList = props => {
+    console.log(props.songs);
     return (
         <ul>
-            {
-             props.songs.map(song => {
-                 return <li>
-                     <a onClick={() =>{
-                         console.log(song.id);
-                         props.handleClick(song.id);
-                     }}>{song.title}
-                     </a>
-                     </li>
-             })   
-            }
+            {props.songs.map(song => {
+                return (
+                    <li>
+                        <a
+                            href
+                            onClick={() => {
+                                console.log(song.id);
+                                props.handleClick(song.id);
+                            }}
+                        >
+                            {song.title}
+                        </a>
+                    </li>
+                );
+            })}
         </ul>
-
-    )
-}
+    );
+};
 
 export default SongList;
